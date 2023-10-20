a03379f3edacbda5e71ed0d8ae8b5f8fb1609d66
Valerii Gassiev <vvv.gassiev@gmail.com>
2023-10-21 00:14:09 +0300

    add GroupedExpense file

---
+++ ExpenseTracker/Model/GroupedExpense.swift
@@ -0,0 +1,18 @@
+//
+//  GroupedExpense.swift
+//  ExpenseTracker
+//
+//  Created by Valera Gassiev on 10/21/23.
+//
+
+import SwiftUI
+
+struct GroupedExpense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupedExpense()
+}
