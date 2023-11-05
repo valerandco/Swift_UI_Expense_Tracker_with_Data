5ac99c8fb19e2c1272a798a80442e2cffc006adf
Valerii Gassiev <vvv.gassiev@gmail.com>
2023-11-05 20:46:53 +0300

    ExpenseCardView

---
+++ ExpenseTracker/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by Valera Gassiev on 11/5/23.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
---
+++ ExpenseTracker/View/ExpenseCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseCardView.swift
+//  ExpenseTracker
+//
+//  Created by Valera Gassiev on 11/5/23.
+//
+
+import SwiftUI
+
+struct ExpenseCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseCardView()
+}
